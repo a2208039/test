3fa73f9e6e86a6423608f84598f50662899eaa18
Iker <154421316+a2208039@users.noreply.github.com>
2024-01-18 17:27:39 +0100

    Update contador3.php

--- contador3.php
+++ contador3.php
@@ -1,13 +1,27 @@
-<?php
-$contador = 0;
-
-if ($fd = fopen('counter.txt', 'r')) {
-    $contador = fgets($fd);
-    fclose($fd);
-}
-
-$fd2 = fopen('counter.txt', 'w');
-$contador = $contador + 1;
-fwrite($fd2, $contador);
-fclose($fd2);
-?>
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contador3</title>
+    </head>
+    <body>
+        <?php
+            $contador = 0;
+            
+            $fd = @fopen('counter.txt', 'r');
+            if ($fd) {
+                $contador = fgets($fd);
+                fclose($fd);
+            
+                $fd2 = @fopen('counter.txt', 'w');
+                if ($fd2) {
+                    $contador = $contador + 1;
+                    fwrite($fd2, $contador);
+                    fclose($fd2);
+                }
+            }
+        ?>
+        
+        <a href="counter.txt" target="_blank"><button>Ver Archivo 'counter.txt'</button></a>
+        
+    </body>
+</html>
